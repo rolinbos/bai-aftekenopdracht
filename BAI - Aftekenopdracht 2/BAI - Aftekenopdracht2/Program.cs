42e66c110eec18db3c6dc8601c5d7ca0a161b659
Rolin Bos <rolinbos1@icloud.com>
2021-10-13 11:26:39 +0200

    Second practice

--- BAI - Aftekenopdracht 2/BAI - Aftekenopdracht2/Program.cs
+++ BAI - Aftekenopdracht 2/BAI - Aftekenopdracht2/Program.cs
@@ -8,66 +8,132 @@ namespace BAI
     {
         public static bool Vooruit(uint b)
         {
+            if ((b & 128) == 128)
+            {
+               return true;
+            }
+
+            //Console.WriteLine($"{Convert.ToString(b, 2).PadLeft(8, '0')}");
+            //Console.WriteLine(b);
             // *** IMPLEMENTATION HERE *** //
             return false;
         }
         public static uint Vermogen(uint b)
         {
+            // Je moet van boven naar beneden werken omdat je na 1 value kijkt
+            // als beiden aanstaan kijkt geeft hij true voor eentje
+            if ((b & 96) == 96)
+            {
+                return 100;
+            } else if ((b & 64) == 64)
+            {
+                return 67;
+            } else if ((b & 32) == 32)
+            {
+                return 33;
+            }
+
             // *** IMPLEMENTATION HERE *** //
             return 0;
         }
         public static bool Wagon(uint b)
         {
+            if ((b & 16) == 16)
+            {
+                return true;
+            }
             // *** IMPLEMENTATION HERE *** //
             return false;
         }
         public static bool Licht(uint b)
         {
+            if ((b & 8) == 8)
+            {
+                return true;
+            }
             // *** IMPLEMENTATION HERE *** //
             return false;
         }
         public static uint ID(uint b)
         {
-            // *** IMPLEMENTATION HERE *** //
-            return 0;
+            return b & 7;
         }
 
         public static HashSet<uint> Alle(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
+
+            for (int i = 0; i < inputStroom.Count; i++)
+            {
+                set.Add(inputStroom[i]);
+            }
+
             // *** IMPLEMENTATION HERE *** //
             return set;
         }
         public static HashSet<uint> ZonderLicht(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
-            // *** IMPLEMENTATION HERE *** //
+
+            for (int i = 0; i < inputStroom.Count; i++)
+            {
+                if (! Licht(inputStroom[i]))
+                {
+                    set.Add(inputStroom[i]);
+                }
+            }
+
             return set;
         }
         public static HashSet<uint> MetWagon(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
-            // *** IMPLEMENTATION HERE *** //
+
+            for (int i = 0; i < inputStroom.Count; i++)
+            {
+                if (Wagon(inputStroom[i]))
+                {
+                    set.Add(inputStroom[i]);
+                }
+            }
+
             return set;
         }
         public static HashSet<uint> SelecteerID(List<uint> inputStroom, uint lower, uint upper)
         {
             HashSet<uint> set = new HashSet<uint>();
-            // *** IMPLEMENTATION HERE *** //
+
+            for (int i = 0; i < inputStroom.Count; i++)
+            {
+                if (ID(inputStroom[i]) >= lower && ID(inputStroom[i]) <= upper)
+                {
+                    set.Add(inputStroom[i]);
+                }
+            }
+
             return set;
         }
 
         public static HashSet<uint> Opg3a(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
-            // *** IMPLEMENTATION HERE *** //
+
+            set = SelecteerID(inputStroom, 0, 2);
+            set.IntersectWith(ZonderLicht(inputStroom));
+
             return set;
         }
 
         public static HashSet<uint> Opg3b(List<uint> inputStroom)
         {
             HashSet<uint> set = new HashSet<uint>();
-            // *** IMPLEMENTATION HERE *** //
+
+            set = Alle(inputStroom);
+            set.ExceptWith(Opg3a(inputStroom));
+            
+            //set = SelecteerID(inputStroom, 2, 8);
+            //set.(ZonderLicht(inputStroom));
+
             return set;
         }
 
